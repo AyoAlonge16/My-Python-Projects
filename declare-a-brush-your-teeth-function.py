09c5fb852ec5edffddd9762c55ce82326f6f2e10
Ayo Alonge <theayoalonge@gmail.com>
2024-03-03 23:11:47 +0100

    defining a funtion

---
+++ declare-a-brush-your-teeth-function.py
@@ -0,0 +1,8 @@
+def brush_teeth():
+  print("take the toothbrush")
+  print("put paste on the brush")
+  print("clean teeth")
+  print("wash the toothbrush")
+  print("rinse mouth")
+brush_teeth()
+
