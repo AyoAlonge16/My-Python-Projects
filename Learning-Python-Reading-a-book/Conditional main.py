e347dc35184175bdc4740d6eeebd97de84e68f8c
AyoAlonge16 <theayoalonge@gmail.com>
2024-03-28 09:08:14 +0100

    more python practise

---
+++ Learning-Python-Reading-a-book/Conditional main.py
@@ -0,0 +1,31 @@
+from __future__ import print_function
+
+'''
+def main():
+    print("Hello World")
+
+if __name__ == "__main__":
+    main()
+'''
+
+'''
+def main():
+	print("We have a question for you!")
+	name = input('Your name: ')
+	print('Hello ' + name + ', how are you?')
+main()
+'''
+
+#A code that works for both python 3 and python 2
+
+
+import sys
+
+def main():
+    if sys.version_info.major < 3:
+        name = raw_input('Your name: ')
+    else:
+        name = input('Your name: ')
+    print('Hello ' + name + ', how are you?')
+    
+main()
